2eb3b61556ead1b818d7da3f0524329174ec9baa
Your Name <you@example.com>
2022-10-06 15:13:42 +0700

    completed

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,9 +1,30 @@
+import { checkToken } from "../../backendLibs/checkToken";
+import { readUsersDB, writeUsersDB } from "../../backendLibs/dbLib";
 export default function summaryRoute(req, res) {
   if (req.method === "GET") {
     //check authentication
-    //return res.status(403).json({ ok: false, message: "Permission denied" });
+    const user = checkToken(req);
+    if (!user || !user.isAdmin)
+      return res.status(403).json({ ok: false, message: "Permission denied" });
     //compute DB summary
+    const users = readUsersDB();
+    let userCount = 0;
+    let adminCount = 0;
+    let totalMoney = 0;
+    users.forEach((x) => {
+      if (x.isAdmin) adminCount++;
+      else {
+        userCount++;
+        totalMoney += x.money;
+      }
+    });
     //return response
+    return res.status(200).json({
+      ok: true,
+      userCount: userCount,
+      adminCount: adminCount,
+      totalMoney: totalMoney,
+    });
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
